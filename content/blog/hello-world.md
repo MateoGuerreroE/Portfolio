ebc55c94892cf382cee34ce7c065839483baf4b3
MateoGuerreroE <mateof1223@gmail.com>
2023-12-05 14:47:04 -0500

    Working on blog posting

---
+++ content/blog/hello-world.md
@@ -0,0 +1,5 @@
+# Hello World!
+
+This is my first blog post
+
+## Heading 2
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/content"],
+  content: {
+    highlight: {
+      theme: "nord",
+      preload: ["ts", "js", "css", "java", "json", "bash", "vue"],
+    },
+  },
   devtools: { enabled: true },
 });
