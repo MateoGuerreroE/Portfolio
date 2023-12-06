ebc1e4b228e271dcf66adc3005f56eba5afeae07
MateoGuerreroE <mateof1223@gmail.com>
2023-12-05 22:49:21 -0500

    Blog data completed

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,8 @@
-<script setup></script>
+<script setup>
+const { data: posts } = await useAsyncData("latest-posts", () =>
+  queryContent("/blog").sort({ data: 1 }).limit(3).find()
+);
+</script>
 
 <template>
   <section>
@@ -33,10 +37,13 @@
     <img
       src="../assets/images/profile.JPG"
       alt="Profile Photo"
-      class="w-1/2 md:max-w-sm p-8 mx-auto"
+      class="w-1/2 h-1/2 md:max-w-sm p-8 mx-auto"
     />
   </section>
   <section>
     <h2 class="text-3xl font-bold mt-8">Latest Blog Posts</h2>
+    <div class="grid md:grid-cols-3 pt-8 gap-5">
+      <Post :posts="posts" />
+    </div>
   </section>
 </template>
